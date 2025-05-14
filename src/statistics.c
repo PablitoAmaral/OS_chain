d02a5b5847f4186bd0092ed4eee453871d41b817
ricardoparede <ricardobmfp@gmail.com>
2025-05-14 18:24:41 +0100

    fix:sigusr1 preparado

--- src/statistics.c
+++ src/statistics.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "statistics.h"
+#include <signal.h>  
+
+void handle_stats_sigusr1(int sig) {
+      // POR IMPLEMENTAR  DAR PRINT A ESTATISTICAS---------------------------------------------------------------------
+  }
+
 
 void run_statistics(void) {
+    signal(SIGUSR1, handle_stats_sigusr1); // SIGUSR1 signal handler
+
     printf("[Statistics] Processo statistics iniciado!\n");
     while (1); // manter ativo para testes
 }
--- src/statistics.h
+++ src/statistics.h
@@ -1,6 +1,7 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
+void handle_stats_sigusr1(int sig);
 void run_statistics(void);
 
 #endif
