2698c840a1abf9e5c1c5821e01154ee7fcf43e7f
ricardoparede <ricardobmfp@gmail.com>
2025-06-20 18:02:02 +0100

    Pow implementado

--- src/ipc_utils.h
+++ src/ipc_utils.h
@@ -4,9 +4,16 @@
 #define IPC_UTILS_H
 
 #include "config.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#define TX_ID_LEN 32
+#define TX_ID_LEN 64
+#define TXB_ID_LEN 64
+#define HASH_SIZE 65  // SHA256_DIGEST_LENGTH * 2 + 1
+
+extern size_t transactions_per_block;
 
 // IDs de memória partilhada
 extern int shm_pool_id;
@@ -19,6 +26,25 @@ typedef struct {
     float value;               // valor da transação
     time_t timestamp;          // timestamp da criação
 } Transaction;
+
+// Transaction Block structure
+typedef struct {
+  char txb_id[TXB_ID_LEN];              // Unique block ID (e.g., ThreadID + #)
+  char previous_block_hash[HASH_SIZE];  // Hash of the previous block
+  time_t timestamp;                     // Time when block was created
+  Transaction *transactions;            // Array of transactions
+  unsigned int nonce;                   // PoW solution
+} TransactionBlock;
+
+// Inline function to compute the size of a TransactionBlock
+static inline size_t get_transaction_block_size() {
+  if (transactions_per_block == 0) {
+    perror("Must set the 'transactions_per_block' variable before using!\n");
+    exit(-1);
+  }
+  return sizeof(TransactionBlock) +
+         transactions_per_block * sizeof(Transaction);
+}
 
 // Entrada na pool de transações
 typedef struct {
